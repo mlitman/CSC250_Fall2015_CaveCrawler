a9548bb9ac23dd532cdf33d14763430b515c57a7
Mike Litman <mike@awesomefat.com>
2015-12-03 11:26:58 -0600

    works for adding rooms

--- Room.java
+++ Room.java
@@ -20,6 +20,27 @@ public class Room
 		}
 	}
 	
+	public Room(String name, int id)
+	{
+		this.name = name;
+		this.id = id;
+		this.thePlayer = null;
+		this.theExits = new Exit[0];
+	}
+	
+	public JSONObject getJSONObject()
+	{
+		JSONObject theObj = new JSONObject();
+		theObj.addVariable(new JSONNumberVariable("id", this.id));
+		theObj.addVariable(new JSONStringVariable("name", this.name));
+		JSONArrayVariable theExits = new JSONArrayVariable("exits");
+		for(Exit e : this.theExits)
+		{
+			theExits.addJSONObject(e.getJSONObject());
+		}
+		theObj.addVariable(theExits);
+		return theObj;
+	}
 	
 	public int getId() 
 	{
@@ -41,16 +62,30 @@ public class Room
 		{
 			if(this.theExits[i].getName().equalsIgnoreCase(exitName))
 			{
-				this.thePlayer.displayToUser("Nice Try!!!");
-//********************
-				//write the code to actually find the Room associated with
-				//the destiantionID of this Exit and then add the player to
-				//that Room
-				//We can find our Rooms in CaveCore now!!!!
-				return true;
+				for(Room r : CaveCore.theRooms)
+				{
+					if(r.getId() == this.theExits[i].getDestinationID())
+					{
+						r.addThePlayer(this.thePlayer);
+						this.thePlayer = null;
+						return true;
+					}
+				}
 			}
 		}
+		this.thePlayer.displayToUser("Destination Room not found!!!");
 		return false;
+	}
+	
+	public void displayDetailsToUser()
+	{
+		this.thePlayer.displayToUser("You have entered: " + this.name);
+		this.thePlayer.displayToUser("Possible Exits: ");
+		for(int i = 0; i < this.theExits.length; i++)
+		{
+			this.thePlayer.displayToUser(this.theExits[i].getName());
+		}
+		this.thePlayer.showPrompt();
 	}
 	
 	public void addThePlayer(Player thePlayer) 
@@ -60,22 +95,18 @@ public class Room
 		
 		//set this room's player to thePlayer and display details to thePlayer
 		this.thePlayer = thePlayer;
-		this.thePlayer.displayToUser("You have entered: " + this.name);
-		this.thePlayer.displayToUser("Possible Exits: ");
-		for(int i = 0; i < this.theExits.length; i++)
-		{
-			this.thePlayer.displayToUser(this.theExits[i].getName());
-		}
-		this.thePlayer.showPrompt();
+		this.displayDetailsToUser();
 	}
 
 
-	public void addExit(String name, Room destination)
+	public void addExit(String name, int destinationID)
 	{
-//*****************
-		//write the code to add an additional Exit to this Room
-		//which involves making the theExits array one bucket
-		//larger and then creating a new Exit and adding it to
-		//the end.
+		Exit[] tempExits = new Exit[this.theExits.length+1];
+		for(int i = 0; i < this.theExits.length; i++)
+		{
+			tempExits[i] = this.theExits[i];
+		}
+		tempExits[tempExits.length-1] = new Exit(name, destinationID);
+		this.theExits = tempExits;
 	}
 }
